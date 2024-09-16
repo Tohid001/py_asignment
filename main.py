c0c78628c4b247523c90ffd094ff69ffdd913f9a
Tohid001 <mdtohid.sust@gmail.com>
2024-09-17 04:44:50 +0600

    fix: fixed issue related to showing helptext of each subcommands

--- main.py
+++ main.py
@@ -11,14 +11,23 @@ def main():
     parser = argparse.ArgumentParser(description="Task Management System")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    add_parser = subparsers.add_parser("add", help="Add a new task")
-    add_parser.add_argument("--title", help="Task title")
-    add_parser.add_argument("--description", help="Task description")
+    add_parser = subparsers.add_parser(
+        "add", help="Add a new task(poetry run python main.py add -h)"
+    )
+    add_parser.add_argument("--title", required=True, help="Task title(required)")
+    add_parser.add_argument(
+        "--description", required=True, help="Task description(required)"
+    )
 
-    complete_parser = subparsers.add_parser("complete", help="Mark a task as completed")
-    complete_parser.add_argument("id", type=str, help="Task UUID")
+    complete_parser = subparsers.add_parser(
+        "complete",
+        help="Mark a task as completed(poetry run python main.py complete -h)",
+    )
+    complete_parser.add_argument("id", type=str, help="Task ID")
 
-    list_parser = subparsers.add_parser("list", help="List all tasks")
+    list_parser = subparsers.add_parser(
+        "list", help="List all tasks(poetry run python main.py list -h)"
+    )
     list_parser.add_argument(
         "--incomplete", action="store_true", help="Show only incomplete tasks"
     )
