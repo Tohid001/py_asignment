a87021c93a7fd667b0fedeb83a8c902239bc1ae9
Tohid001 <mdtohid.sust@gmail.com>
2024-09-17 02:54:31 +0600

    fix: adding unique id as updating by title may conflict due to duplication

--- task_manager.py
+++ task_manager.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+import uuid
 
 
 class Task:
 
     def __init__(self, title, description):
+        self.id = str(uuid.uuid4())  # Generate a unique UUID
         self.title = title
         self.description = description
         self.completed = False
